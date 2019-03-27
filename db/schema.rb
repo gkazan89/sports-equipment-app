6c9ef0faa12c673facd5820ecf7f6a4302d1002e
Grant Kazan <grantkazan@gmail.com>
2019-03-26 19:36:59 -0500

    changed price from int to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_233032) do
+ActiveRecord::Schema.define(version: 2019_03_27_003510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2019_03_26_233032) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "sport"
-    t.integer "price"
+    t.decimal "price", precision: 5, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
