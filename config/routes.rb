0d58b53bbe6978d5b1e8b61e54bfec3387f314a2
Grant Kazan <grantkazan@gmail.com>
2019-04-06 17:13:55 -0500

    changed order information route to post request

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
     get "/carted_items" => "carted_items#index"
     post "/carted_items" => "carted_items#create"
     delete "/carted_items/:id" => "carted_items#destroy"
-    get "/carted_items/details" => "carted_items#order_details"
+    post "/carted_items/details" => "carted_items#order_details"
 
 
     get "/orders" => "orders#index"
